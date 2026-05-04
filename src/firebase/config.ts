8333935bf3eadcffa4e01085c45286dd72648b2f
Flowzonic <flowzonicsolutions@gmail.com>
2026-05-04 10:11:15 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4870316769-10672",
+  "appId": "1:191544860029:web:0138ef06aec94e91ce8014",
+  "apiKey": "AIzaSyBX_yCu5NiWQ590_iaPe3c072vgprjYzn0",
+  "authDomain": "studio-4870316769-10672.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "191544860029"
+};
